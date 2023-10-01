31c29335e2107516ea5e84e80b76379c98285f0d
Chester <chester44311@gmail.com>
2023-10-01 17:47:40 +0800

    Create login service
    - create service for login

--- loginApplicationBE/src/main/java/com/example/loginApplicationBE/service/LoginService.java
+++ loginApplicationBE/src/main/java/com/example/loginApplicationBE/service/LoginService.java
@@ -1,4 +1,10 @@
 package com.example.loginApplicationBE.service;
 
+import com.example.loginApplicationBE.dtos.request.LoginDto;
+import com.example.loginApplicationBE.dtos.response.LoginMessageDto;
+import com.example.loginApplicationBE.dtos.response.UserDetailsDto;
+
 public interface LoginService {
+
+    LoginMessageDto loginUser (LoginDto loginRequest);
 }
--- loginApplicationBE/src/main/java/com/example/loginApplicationBE/service/LoginServiceImpl.java
+++ loginApplicationBE/src/main/java/com/example/loginApplicationBE/service/LoginServiceImpl.java
@@ -1,14 +1,38 @@
 package com.example.loginApplicationBE.service;
 
+import com.example.loginApplicationBE.dtos.request.LoginDto;
+import com.example.loginApplicationBE.dtos.response.LoginMessageDto;
+import com.example.loginApplicationBE.dtos.response.UserDetailsDto;
+import com.example.loginApplicationBE.model.Users;
 import com.example.loginApplicationBE.repository.UsersRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Log4j2
 public class LoginServiceImpl implements LoginService{
 
     @Autowired
     UsersRepository usersRepository;
 
+    public LoginMessageDto loginUser (LoginDto loginRequest) {
+        log.info("BEGIN login for user " + loginRequest.getUsername());
+        LoginMessageDto loginMessageDto = new LoginMessageDto();
 
+        UserDetailsDto userDetailsDto = new UserDetailsDto();
+
+        Optional<Users> usersOptional = usersRepository.findByUsernameAndPassword(loginRequest.getUsername(), loginRequest.getPassword());
+
+        if (usersOptional.isPresent()) {
+
+        } else {
+            loginMessageDto.setMessage("User does not exists");
+            loginMessageDto.setStatus("Failed");
+        }
+
+        return loginMessageDto;
+    }
 }
