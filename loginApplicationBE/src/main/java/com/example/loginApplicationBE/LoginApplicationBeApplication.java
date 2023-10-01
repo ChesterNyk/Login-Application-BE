0a1d46bbc2658deb0fe1797384931ea0c771d4c3
Chester <chester44311@gmail.com>
2023-10-01 15:41:59 +0800

    inital comit

---
+++ loginApplicationBE/src/main/java/com/example/loginApplicationBE/LoginApplicationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginApplicationBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplicationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplicationBeApplication.class, args);
+	}
+
+}
---
+++ loginApplicationBE/src/test/java/com/example/loginApplicationBE/LoginApplicationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginApplicationBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
